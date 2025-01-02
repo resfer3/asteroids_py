b4536436eaeacb10fadcf4545d8ccb0c4214d66b
resfer3 <resfer333@gmail.com>
2025-01-01 17:20:17 -0700

    added rate limit to bullets

--- player.py
+++ player.py
@@ -17,6 +17,7 @@ class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS) 
     self.rotation = 0
+    self.timer = 0
 
   def triangle(self):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -33,6 +34,7 @@ class Player(CircleShape):
     self.rotation += PLAYER_TURN_SPEED * dt
 
   def update(self, dt):
+    self.timer -= dt
     keys = pygame.key.get_pressed()
     inverse = dt * -1
 
@@ -45,7 +47,9 @@ class Player(CircleShape):
     if keys[pygame.K_s]:
       self.move(inverse)
     if keys[pygame.K_SPACE]:
-      self.shoot()
+      if self.timer <= 0:
+        self.shoot()
+        self.timer = PLAYER_SHOOT_COOLDOWN
 
   def move(self, dt):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
